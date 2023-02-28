8ccc858775e461d39c433193126f34df708213d0
Alex <best1972@list.ru>
2023-02-28 09:38:03 +0300

    start task 2

--- Tasks.py
+++ Tasks.py
@@ -3,24 +3,33 @@
 # заданном списке искомое число X, которое мы вводим с клавиатуры, либо выводим на экран, 
 # максимально близкое ему по значению
 
-import random
+# import random
 
-lengthyList = int(input('Введите длину списка: '))
-myList = [random.randint(1, 100) for _ in range (lengthyList)]
+# lengthyList = int(input('Введите длину списка: '))
+# myList = [random.randint(1, 100) for _ in range (lengthyList)]
 
-print(myList)
+# print(myList)
 
-requiredNumber = int(input('Введите искомое число от 1 до 100: '))
-count = myList.count(requiredNumber)
-requiredNumberNew = myList[0]
+# requiredNumber = int(input('Введите искомое число от 1 до 100: '))
+# count = myList.count(requiredNumber)
+# requiredNumberNew = myList[0]
 
-if count > 0:
-    print(count)
-else:
-    for item in myList:
-        if abs(item - requiredNumberNew) < abs(requiredNumberNew - requiredNumber):
-            requiredNumberNew = item
-    print (f'Искомое число не найдено, максимально близкое ему по значению: {requiredNumberNew}')        
+# if count > 0:
+#     print(count)
+# else:
+#     for item in myList:
+#         if abs(item - requiredNumberNew) < abs(requiredNumberNew - requiredNumber):
+#             requiredNumberNew = item
+
+#     print (f'Искомое число не найдено, максимально близкое ему по значению: {requiredNumberNew}')        
     
-
+#******************************************************************************************
+# Вторая задача: В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность. В случае 
+# с английским алфавитом очки распределяются так: A, E, I, O, U, L, N, S, T, R – 1 очко; D, G – 2 очка;
+# B, C, M, P – 3 очка; F, H, V, W, Y – 4 очка; K – 5 очков; J, X – 8 очков; Q, Z – 10 очков.
+# А русские буквы оцениваются так: А, В, Е, И, Н, О, Р, С, Т – 1 очко; Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка; Й, Ы – 4 очка; Ж, З, Х, Ц, Ч – 5 очков; Ш, Э, Ю – 8 очков; Ф, Щ, Ъ – 10 очков
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова. Будем считать, 
+# что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы. 
+# ноутбук     12
 
